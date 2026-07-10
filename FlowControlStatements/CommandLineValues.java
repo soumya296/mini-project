429c7e168e111a51c3f2cefee59dfb2fdbabe69e
Soumya <bodepudinagasoumya@gmail.com>
2026-07-10 16:54:03 +0530

    Added Flow Control assignments 1-5

---
+++ FlowControlStatements/CommandLineValues.java
@@ -0,0 +1,18 @@
+public class CommandLineValues {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("No Values");
+        } else {
+
+            for (int i = 0; i < args.length; i++) {
+
+                if (i == args.length - 1)
+                    System.out.print(args[i]);
+                else
+                    System.out.print(args[i] + ",");
+            }
+        }
+    }
+}
